392ba4b694398461a91699f8c4686db52cef3889
sebastian <sebagallegos2@gmail.com>
2023-08-01 08:43:45 -0400

    Ejemplo de rutas

--- html/html.go
+++ html/html.go
@@ -12,15 +12,27 @@ var files embed.FS
 
 // TODO: parse page names
 var (
-	index = parse("index.html")
+	index   = parse("index.html")
+	pagina1 = parse("pagina1.html")
+	pagina2 = parse("pagina2.html")
 )
 
 type IndexParams struct {
 	Title string
 }
 
+// Páginas
+
 func Index(w io.Writer, p IndexParams) error {
 	return index.Execute(w, p)
+}
+
+func Pagina1(w io.Writer, p IndexParams) error {
+	return pagina1.Execute(w, p)
+}
+
+func Pagina2(w io.Writer, p IndexParams) error {
+	return pagina2.Execute(w, p)
 }
 
 var funcs = template.FuncMap{
--- main.go
+++ main.go
@@ -9,7 +9,20 @@ func index(w http.ResponseWriter, r *http.Request) {
 	p := html.IndexParams{Title: "Inicio"}
 	html.Index(w, p)
 }
+
+func pagina1(w http.ResponseWriter, r *http.Request) {
+	p := html.IndexParams{Title: "Página 1"}
+	html.Pagina1(w, p)
+}
+
+func pagina2(w http.ResponseWriter, r *http.Request) {
+	p := html.IndexParams{Title: "Página 2"}
+	html.Pagina2(w, p)
+}
+
 func main() {
 	http.HandleFunc("/", index)
+	http.HandleFunc("/1", pagina1)
+	http.HandleFunc("/2", pagina2)
 	http.ListenAndServe(":8080", nil)
 }
